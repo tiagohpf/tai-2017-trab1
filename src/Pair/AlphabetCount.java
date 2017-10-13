0653b44c6fae0659850ebf48e73cf7842b81f754
Tiago Faria <tiaghpf@ua.pt>
2017-10-13 16:21:07 +0100

    More Comments

--- src/Pair/AlphabetCount.java
+++ src/Pair/AlphabetCount.java
@@ -14,23 +14,49 @@ public class AlphabetCount {
     private String letter;
     private int number;
 
+    /**
+     * Constructor
+     *
+     * @param letter
+     * @param number
+     */
     public AlphabetCount(String letter, int number) {
         this.letter = letter;
         this.number = number;
     }
 
+    /**
+     * Get letter
+     *
+     * @return letter
+     */
     public String getLetter() {
         return letter;
     }
 
+    /**
+     * Get number
+     *
+     * @return number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Change the number
+     *
+     * @param number
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Print the object
+     *
+     * @return string
+     */
     public String toString() {
         return "{" + letter + ", " + number + "}";
     }
--- src/Pair/AlphabetProb.java
+++ src/Pair/AlphabetProb.java
@@ -14,19 +14,40 @@ public class AlphabetProb {
     private String letter;
     private double prob;
 
+    /**
+     * Constructor
+     *
+     * @param letter
+     * @param prob
+     */
     public AlphabetProb(String letter, double prob) {
         this.letter = letter;
         this.prob = prob;
     }
 
+    /**
+     * Get letter
+     *
+     * @return letter
+     */
     public String getLetter() {
         return letter;
     }
 
+    /**
+     * Get probability
+     *
+     * @return prob
+     */
     public double getProb() {
         return prob;
     }
 
+    /**
+     * Print the object
+     *
+     * @return string
+     */
     public String toString() {
         return "{" + letter + ", " + prob + "}";
     }
